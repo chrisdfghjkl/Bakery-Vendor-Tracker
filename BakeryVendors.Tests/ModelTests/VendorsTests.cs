562129ec60a95e5671e06d7a57adb217d2d4ad31
chrisdfghjkl <cdepastene@gmail.com>
2022-02-24 14:45:15 -0800

    add vendor test 1

--- BakeryVendors.Tests/ModelTests/VendorsTests.cs
+++ BakeryVendors.Tests/ModelTests/VendorsTests.cs
@@ -8,8 +8,15 @@ namespace BakeryVendors.Tests
   [TestClass]
   public class VendorsTests
   {
-  
+    [TestMethod]
+    public void GetVendorName_ReturnsVendorName_String()
+    {
+      string vendorName = "New Vendor";
+      Vendors newVendor = new Vendors(vendorName);
 
-    
+      string testVendor = newVendor.vendorName;
+
+      Assert.AreEqual(vendorName, testVendor);
+    }
   }
 }
