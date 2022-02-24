4f2c03766de3438b069f0364f1e4dc10ddbd7d47
chrisdfghjkl <cdepastene@gmail.com>
2022-02-24 15:01:34 -0800

    add business logic passing vendor test 1

--- BakeryVendors/Models/Vendors.cs
+++ BakeryVendors/Models/Vendors.cs
@@ -4,6 +4,11 @@ namespace BakeryVendors.Models
 {
   public class Vendors
   {
-    
+    public string VendorName { get; set; }
+
+    public Vendors(string vendorName)
+    {
+      VendorName = vendorName;
+    }
   }
 }
