689c4b24c83125002611b245ef6d824ea71760d9
chrisdfghjkl <cdepastene@gmail.com>
2022-02-24 18:58:29 -0800

    add boilerplate code

---
+++ BakeryVendors/Controllers/VendorsController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using BakeryVendors.Models;
+using System.Collections.Generic;
+using System;
+
+namespace BakeryVendors.Controllers
+{
+  public class VendorsController : Controller
+  {
+    
+  }
+}
