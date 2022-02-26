297e686b6a0bac2ad0f4027ced43ff57f957d671
chrisdfghjkl <cdepastene@gmail.com>
2022-02-26 15:45:58 -0800

    Add new and show routes

--- BakeryVendors/Controllers/OrderController.cs
+++ BakeryVendors/Controllers/OrderController.cs
@@ -6,6 +6,22 @@ namespace BakeryVendors.Controllers
 {
   public class OrderController : Controller
   {
-    
+    [HttpGet("/vendors/{vendorId}/orders/new")]
+    public ActionResult New(int VendorId)
+    {
+      Vendors vendor = Vendors.Find(VendorId);
+      return View(vendor);
+    }
+
+    [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+    public ActionResult Show(int VendorId, int OrderId)
+    {
+      Order order = Order.Find(OrderId);
+      Vendors vendor = Vendors.Find(VendorId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("order", order);
+      model.Add("vendor", vendor);
+      return View(model);
+    }
   }
 }
