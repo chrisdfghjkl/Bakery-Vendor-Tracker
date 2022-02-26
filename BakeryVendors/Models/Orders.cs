7a863da8aee5bc6e071ee6d7b9f2c24fbe18d3be
chrisdfghjkl <cdepastene@gmail.com>
2022-02-26 01:41:09 -0800

    add passing order test 4

--- BakeryVendors/Models/Orders.cs
+++ BakeryVendors/Models/Orders.cs
@@ -8,10 +8,13 @@ namespace BakeryVendors.Models
     private static List<Order> _orderInstances = new List<Order> {};
     public string OrderTitle { get; set;}
     public string OrderDescription { get; set; }
-    public Order(string orderTitle, string orderDescription)
+    public int OrderPrice { get; set; }
+    public Order(string orderTitle, string orderDescription, int orderPrice)
     {
       OrderTitle = orderTitle;
       OrderDescription = orderDescription;
+      OrderPrice = orderPrice;
+      _orderInstances.Add(this);
     }
     public static void ClearAll()
     {
