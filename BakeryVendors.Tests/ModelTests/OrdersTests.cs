12da23953abf3f5f73614eed2e0ceabcb8d992ed
chrisdfghjkl <cdepastene@gmail.com>
2022-02-25 21:44:11 -0800

    add order test 3

--- BakeryVendors.Tests/ModelTests/OrdersTests.cs
+++ BakeryVendors.Tests/ModelTests/OrdersTests.cs
@@ -26,5 +26,17 @@ namespace BakeryVendors.Tests
 
       Assert.AreEqual(title, orderTitle);
     }
+
+    [TestMethod]
+    public void GetDescription_ReturnsOrderDescription_String()
+    {
+      string orderTitle = "Cafe order 1";
+      string orderDescription = "ten bread loaves, 30 pastries";
+      Order newOrder = new Order(orderTitle, orderDescription);
+
+      string details = newOrder.OrderDescription;
+
+      Assert.AreEqual(details, orderDescription);
+    }
   }
 }
